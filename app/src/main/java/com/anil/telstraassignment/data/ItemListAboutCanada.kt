90c62f10ff11d62f4631e48b6bae18b3d7a5e6b6
anil-wipro <anil.jha1@wipro.com>
2019-08-22 15:09:28 +0530

    added data classes for expected response and rx java dependency for api call

---
+++ app/src/main/java/com/anil/telstraassignment/data/ItemListAboutCanada.kt
@@ -0,0 +1,6 @@
+package com.anil.telstraassignment.data
+
+data class ItemListAboutCanada(
+    private val title: String,
+    private val description: String,
+    private val imageHref: String)
---
+++ app/src/main/java/com/anil/telstraassignment/ui/aboutcanada/AboutCanadaAdapter.kt
@@ -0,0 +1,25 @@
+package com.anil.telstraassignment.ui.aboutcanada
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class AboutCanadaAdapter : RecyclerView.Adapter<AboutCanadaAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
