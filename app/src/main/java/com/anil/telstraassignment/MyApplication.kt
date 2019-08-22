88ee77606229d46698801a74e51db28db9e66d6f
anil-wipro <anil.jha1@wipro.com>
2019-08-22 11:00:06 +0530

    setup basic project structure

---
+++ app/src/main/java/com/anil/telstraassignment/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.anil.telstraassignment
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/anil/telstraassignment/ui/aboutcanada/AboutCanadaActivity.kt
@@ -0,0 +1,13 @@
+package com.anil.telstraassignment.ui.aboutcanada
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.anil.telstraassignment.R
+
+class AboutCanadaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aboutcanada)
+    }
+}
